570f96cf98d0e3b6fc177d94928b485263a87f5a
Naveen Bantu <naveen.bantu1@gmail.com>
2022-07-10 09:59:34 +0200

    Update DarkModeButton.jsx

--- toggle-dark-mode/src/components/DarkModeButton.jsx
+++ toggle-dark-mode/src/components/DarkModeButton.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const DarkModeButton = (darkTheme) => {
+
+// Dark mode Button which helps toggle the theme between dark and light
+// Take in the state variable 'darkTheme' and the state setting function 'setDarkTheme'
+
+const DarkModeButton = ({darkTheme, setDarkTheme}) => {
   
   return(       
          <button type='button' className='text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg' onClick={() => setDarkTheme(!darkTheme)}>
